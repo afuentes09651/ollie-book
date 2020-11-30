9bf127ba19b49aa31dfbb35b4d0956a81aacc8a6
Armando <armandez09651@yahoo.com>
2020-11-30 17:22:15 -0600

    Init and then some (well just a tiny bit extra)

---
+++ src/components/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Nav = () => (
+  <nav>
+    <button>Home</button>
+    <button>About</button>
+    <button>Kitty</button>
+    <button>idk</button>
+  </nav>
+)
+
+export default Nav
