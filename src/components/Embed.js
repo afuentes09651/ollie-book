d20480d48de730a9e7316017331bbb5b4d62ed88
Armando <armandez09651@yahoo.com>
2021-01-01 22:12:42 -0600

    Simple Test on embedded Swtiches/Routes. Need to improve on it later

---
+++ src/components/Embed.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Embed = () => (
+  <p>Embed her? I hardly know her!</p>
+);
+
+export default Embed;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
+import Embed from './Embed';
 
 const Home = () => (
-  <p>This is home </p>
+  <>
+  <h2>Hello there, Ollie lover!</h2>
+  <div class='info'>
+    <img src={require('../photos/ewok.png')} />
+    <p>This is the webpage for all things Ollie. Feel free to like and subscribe!</p>
+      <div>
+        <Link to='/Home/Embed'>
+          <button>idk</button>
+        </Link>
+        <Switch>
+          <Route path='/Home/Embed' component={Embed}/>
+        </Switch>
+      </div>
+  </div>
+  </>
 )
 
 export default Home
